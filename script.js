88fb82b1477b846761b38660b661fe8e4f6597dc
JayrajkumarSarvaiya <jayrajkumar.sarvaiya@indexnine.com>
2021-12-17 18:24:12 +0530

    Game Created in Console

--- script.js
+++ script.js
@@ -17,39 +17,24 @@ function playRound(playerSelection, computerSelection) {
       (playerSelection == 'paper' && computerSelection == 'rock'))
     {
       playerScore += 1;
-      result = ('You win ' + playerSelection + ' Bets ' + computerSelection + "<br><br>Player Score :" + playerScore  + "<br>Computer score: " + computerScore);
+      result = ('You win ' + playerSelection + ' Bets ' + computerSelection + " Player Score : " + playerScore  + " Computer score: " + computerScore);
       console.log(result);
-            //document.getElementById('result').innerHTML = result;
     }
     else if (playerSelection == computerSelection) {
       result = ('It\'s a tie. You both chose ' + playerSelection
-          + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+          + " Player score: " + playerScore + " Computer score: " + computerScore);
           console.log(result);
-          //document.getElementById('result').innerHTML = result;
     }
     else {
       computerScore += 1
       result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
-          + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+          + " Player score: " + playerScore + " Computer score: " + computerScore)
           console.log(result);
-          //document.getElementById('result').innerHTML = result;
     }
-    if(computerScore > playerScore)
-    {
-     console.log("Sorry ..!! Batter Luck Next Time..!!");
-     let y = 'Sorry ..!! Batter Luck Next Time..!!';
-     document.getElementById('win').innerHTML = y;
-     document.getElementById('result').innerHTML = result
-    }
-    else if(computerScore < playerScore)
-    {
-     console.log("Congratulations..!!! You Win The Game !!");
-     let x = 'Congratulations..!!! You Win The Game !!';
-     document.getElementById('win').innerHTML = x;
-     document.getElementById('result').innerHTML = result
-    }
+   
     return
 }
+function game (){
   for (let index = 1; index < 6; index++) {
     //console.log("i:",index);
     
@@ -58,3 +43,19 @@ function playRound(playerSelection, computerSelection) {
   const computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
   }
+  if(computerScore > playerScore)
+  {
+   console.log("Sorry ..!! Batter Luck Next Time..!!");
+   let y = 'Sorry ..!! Batter Luck Next Time..!!';
+   document.getElementById('win').innerHTML = y;
+   //document.getElementById('result').innerHTML = result;
+  }
+  else if(computerScore < playerScore)
+  {
+   console.log("Congratulations..!!! You Win The Game !!");
+   let x = 'Congratulations..!!! You Win The Game !!';
+   document.getElementById('win').innerHTML = x;
+   //document.getElementById('result').innerHTML = result;
+  }
+}
+console.log(game())
